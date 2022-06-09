cb21a8647d9c84e947f6f8502d25cf56ee595983
Fernando Monrreal <fmonrreal@vothsolutions.com>
2022-06-09 15:41:16 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const server = require('http').Server(app);
+const socketio = require('socket.io')(server);
+
+app.set('port',process.env.PORT || 3000);
+
+//Ejecutamos la funcion de sockets.js
+require('./sockets')(socketio);
+
+//Archivos static
+
+app.use(express.static(path.join(__dirname,'public')))
+
+server.listen(app.get('port'), () => {
+    console.log('Servidor en el puerto',app.get('port'));
+
+})
